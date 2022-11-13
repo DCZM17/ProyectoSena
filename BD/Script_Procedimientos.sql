91ef8b24f26fd6c5749053b618e35da71f438f57
DCZM17 <dczapata55@misena.edu.co>
2022-11-13 10:57:48 -0500

    Creacion automatica de contraseña

--- BD/Script_Procedimientos.sql
+++ BD/Script_Procedimientos.sql
@@ -1,11 +1,12 @@
 /*Proceimiento Insertar*/
+USE bdacarreosgutierrez;
 DELIMITER //
 CREATE PROCEDURE add_Empleado(
 IN _correoUsuario VARCHAR(30),
 IN _rolUsuario VARCHAR(30),
 IN _estadoUsuario BOOLEAN,
 IN _tipoDocEmpleado VARCHAR(30),
-IN _docEmpleado VARCHAR(30),
+IN _docEmpleado VARCHAR(10),
 IN _nombreEmpleado VARCHAR(30),
 IN _apellidoEmpleado VARCHAR(30),
 IN _celularEmpleado VARCHAR (15),
@@ -13,11 +14,21 @@ IN _direccionEmpleado VARCHAR(100),
 IN _fotoEmpleado VARCHAR(100)
 )
 BEGIN
-    INSERT INTO Usuario (correoUsuario,  rolUsuario, estadoUsuario) VALUES(_correoUsuario,  _rolUsuario, _estadoUsuario);
+DECLARE aux VARCHAR (30)  DEFAULT ' ';
+DECLARE digitos VARCHAR(5) DEFAULT ' ';
+DECLARE letraN VARCHAR(1) DEFAULT ' ';
+DECLARE letraA1 VARCHAR(1) DEFAULT ' ';
+DECLARE contrasenaUsuario VARCHAR(30) DEFAULT ' ';
+
+SET aux = CONCAT('',_docEmpleado);
+SET digitos = RIGHT(aux,5);
+SET letraN = UPPER(LEFT(_nombreEmpleado,1) );
+SET letraA1 = LOWER(LEFT(_apellidoEmpleado,1) );
+SET contrasenaUsuario = CONCAT (digitos,letraN,'-',letraA1); 
+    INSERT INTO Usuario (correoUsuario,contrasenaUsuario,  rolUsuario, estadoUsuario) VALUES(_correoUsuario,contrasenaUsuario,  _rolUsuario, _estadoUsuario);
     INSERT INTO Empleado (tipoDocEmpleado,docEmpleado, nombreEmpleado, apellidoEmpleado, celularEmpleado, direccionEmpleado,fotoEmpleado,idUsuario) VALUES(_tipoDocEmpleado, _docEmpleado, _nombreEmpleado, _apellidoEmpleado, _celularEmpleado, _direccionEmpleado,_fotoEmpleado,  (SELECT idUsuario FROM usuario ORDER BY idUsuario DESC LIMIT 1));
 END//
-
-CALL add_Empleado('ducadianis1@hotmail.com','Conductor',TRUE,'ce','101903851','Diana','Reyes','3134470215','carrera 82 # 39a 14','5');
+CALL add_Empleado('ducadianis1@hotmail.com','Conductor',TRUE,'CC',1019038350,'Diana','Reyes','3134470215','carrera 82 # 39a 14','5');
 
 /*Proceimiento Actualizar*/
 DELIMITER //
